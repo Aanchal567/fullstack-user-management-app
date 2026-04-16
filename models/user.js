3034872d15448ca4f6e8fd8a9ea886e0c93dab66
Aanchal Verma <aanchalv201@gmail.com>
2026-04-16 16:16:30 +0530

    fix db connection properly

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // MongoDB Atlas connection
-mongoose.connect("mongodb+srv://aanchal:123@cluster0.deaqzkf.mongodb.net/testapp1")
+mongoose.connect("mongodb+srv://aanchal:123@cluster0.deaqzkf.mongodb.net/testapp1?retryWrites=true&w=majority")
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 // Schema
